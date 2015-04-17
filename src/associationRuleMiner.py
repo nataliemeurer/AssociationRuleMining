a1c2b3dae65dfa10caf8c964a5782d5d7df81e0e
Kevin Meurer <kevin.a.meurer@gmail.com>
2015-04-17 18:45:17 -0400

    Fix initial implementation of frequency calculations.

--- src/associationRuleMiner.py
+++ src/associationRuleMiner.py
@@ -1,5 +1,7 @@
 import Queue as qu
 import settings as ENV
+import itertools as it
+import utils as util
 
 
 class AssociationRuleMiner:
@@ -11,6 +13,8 @@ class AssociationRuleMiner:
 
 	def generateRules(self):
 		itemSets = self.generateFrequentItemsets()
+		print "\n"
+		print itemSets
 		self.buildRules()
 		print "hola"
 
@@ -21,29 +25,68 @@ class AssociationRuleMiner:
 		supports = {}
 		# Create a set with each item
 		for idx, item in enumerate(self.products):
-			itemSets.append(set([idx]))
+			currentItemSets.append(set([idx]))
 
 		kVal = 1
-		while len(itemSets) != 0
+		while len(currentItemSets) != 0:
 			# Determine which item sets are frequent within itemSets
+			print "\nDetermining support for itemsets of length " + str(kVal)
+			util.updateProgress(0)
+			freqItemSets = []
 			# for every item in our itemsets, C_k
-			for idx, set in enumerate(currentItemSets):
+			for idx, currentSet in enumerate(currentItemSets):
+				util.updateProgress(float(idx) /float(len(currentItemSets)))
 				# set count to zero
 				count = 0
 				for trans in self.transactions:
 					# If our set is in the transaction
-					if set.issubset(trans)
+					if currentSet.issubset(trans):
 						count += 1
 				support = calculateSupport(count, len(self.transactions))
+				listSet = list(item)
+				listSet.sort()
+				key = ""
+				for num in listSet:
+					key += str(num)
+				supports[key] = support
+				# check if it is within the range we're looking for
 				if support > ENV.MIN_SUPPORT:
-					freqItemSets.append(set)
+					freqItemSets.append(currentSet)
+
+			util.updateProgress(1)
 			# Recalculate our currentItemSets
 			currentItemSets = []
-			
+			i = 0
 
-			kVal += 1
+			print "\nCombining frequent item sets for next stage"
+			util.updateProgress(0)
 			
-			# for idx, item in self.products:
+			while i < len(freqItemSets):
+				util.updateProgress(float(i) / float(len(freqItemSets)))
+				j = i + 1
+				while j < len(freqItemSets):
+					# self-join our two sets
+					unionSet = freqItemSets[i].union(freqItemSets[j])
+					if unionSet not in currentItemSets and len(unionSet) == kVal + 1:
+						currentItemSets.append(unionSet)
+					# increment j
+					j += 1
+				# increment i
+				i += 1
+			# prune our tree
+			indicesToRemove = []
+			for idx, currentSet in enumerate(currentItemSets):
+				for iterObj in it.combinations(list(currentSet), kVal):
+					if set(iterObj) not in freqItemSets:
+						# Add the index to our list of indices to be removed
+						indicesToRemove.insert(0, idx)
+						# break and exit
+						break
+			for index in indicesToRemove:
+				if len(currentItemSets) > 0:
+					currentItemSets.pop(index)
+			kVal += 1
+		return freqItemSets
 
 
 	def buildRules(self):
